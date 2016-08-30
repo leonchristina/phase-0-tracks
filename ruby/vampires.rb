d0fb8d2a0a52effd51e86f6bc1eca8f4835d0202
leonchristina <chleon2692@gmail.com>
2016-08-29 17:16:14 -0700

    added vampire.rb modifications

---
+++ ruby/vampires.rb
@@ -0,0 +1,41 @@
+def customers(1)
+
+puts "How many employes will be processed?"
+	employes = gets.chomp
+	if employe < 1 
+class	
+puts "What is your name?"
+	name = gets.chomp
+puts "Please state your birth year"
+	year = gets.chomp
+
+puts "Please state your age"
+	age = gets.chomp 
+
+	if age - year = age
+		age = true
+	else
+		age = false
+	end
+puts "Shall we order you our special garlic bread?"
+	garlic = gets.chomp
+if garlic = y
+	garlic = true
+else
+	garlic = false
+end
+puts "Would you like to enroll in the company's health insurance"
+	insurance = gets.chomp
+
+if insurance = y 
+	insurance = true
+else
+	insurance = false
+end
+
+if age & garlic & insurance == y
+	puts "Probably not a vampire."
+else puts "Probably a vampire"
+end
+
+puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends"
